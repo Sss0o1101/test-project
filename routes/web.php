409ab87051574ff96e48e3fb01d4eb27db026dfc
alias <matusoyo2436@gmail.com>
2024-09-19 15:09:05 +0900

    added new text
       Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
    
    + Route::post('post', [PostController::class, 'store'])->name('post.store');
    
       Route::get('/', function () {
           return view('welcome');
       });

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ Route::get('/test', [TestController::class, 'test'])
 
 Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
 
+Route::post('post', [PostController::class, 'store'])->name('post.store');
+
 Route::get('/', function () {
     return view('welcome');
 });
